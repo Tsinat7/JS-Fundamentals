45108c1645476b4f4aba4b719d520dbaf259591b
Tsinat7 <tsnat100@gmail.com>
2025-05-22 23:18:35 +0300

    clean version of 4-concat.js with const and correct format

--- 4-concat.js
+++ 4-concat.js
@@ -1,3 +1,3 @@
-const arg1 = ProcessingInstruction.argv[2];
-const arg2 = ProcessingInstruction.argv[3];
+const arg1 = Process.argv[2];
+const arg2 = Process.argv[3];
 console.log( '${arg1} is ${arg2}');
