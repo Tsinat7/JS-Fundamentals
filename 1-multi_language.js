49250ec09b1c93a76a2ee0be72d518f080dd02b5
Tsinat7 <tsnat100@gmail.com>
2025-05-22 20:46:48 +0300

    Print 3 lines using console. log with out var

--- 1-multi_language.js
+++ 1-multi_language.js
@@ -1,4 +1,4 @@
-const myVar= "C is fun"
-const myVar= "Python is cool"
-const myVar= "JavaScript is amazing"
-console.log(myVar)
+const myVar= ("C is fun")
+const myVar= ("Python is cool")
+const myVar= ("JavaScript is amazing")
+console.log(myVar))
